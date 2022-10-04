1ff1f03ea404301bfdf55fdcd571b3ea0697abf1
Clara Bernerd <clara.bernerd@etu.univ-grenoble-alpes.fr>
2022-10-04 22:25:17 +0200

    Essaie implémentation MessageQueue

--- SAR1/src/messageQueue/MessageQueue.java
+++ SAR1/src/messageQueue/MessageQueue.java
@@ -1,21 +1,62 @@
 package messageQueue;
 
-abstract class MessageQueue {
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class MessageQueue {
+	
+	private Channel channel;
+	private boolean close;
+	
+	MessageQueue (Channel channel){
+		this.close = false;
+		this.channel = channel;
+		
+	}
 	
 	void send(byte[] bytes, int offset, int length) {
-
+		byte[] messageLength = ByteBuffer.allocate(Integer.BYTES).putInt(length).array();
+		int count = 0;
+		try {
+			count = this.channel.write(messageLength, 0, Integer.BYTES);
+			count += this.channel.write(bytes, 0, length);
+			if(count != length+Integer.BYTES) {
+				throw new IOException("Le nombre de byte n'est pas bon");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	byte[] receive() {
-		return null;
+		byte[] bytes = new byte[64];
+		
+		try {
+			int res = this.channel.read(bytes, 0, Integer.BYTES);
+			if(res != Integer.BYTES) {
+				throw new IOException("Ne peut pas lire la longueur");
+			}
+			int length = (bytes[0]<<24)&0xff000000 |
+					(bytes[1]<<16)&0x00ff0000 |
+					(bytes[2]<<8)&0x0000ff00 |
+					(bytes[3]<<0)&0x000000ff;
+			res = this.channel.read(bytes, 4, length);
+			return bytes;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
-
 	void close() {
-
+		this.close = true;
 	}
 
 	boolean closed() {
-		return false;
+		return this.closed();
 
 	}
 
--- SAR1/src/messageQueue/QueueBroker.java
+++ SAR1/src/messageQueue/QueueBroker.java
@@ -1,15 +1,45 @@
 package messageQueue;
 
-abstract class QueueBroker {
+import java.util.HashMap;
+
+public class QueueBroker {
+		
+	private Broker broker;
+	private HashMap<Integer, MessageQueue> messagequeue;
+	
+	
 	QueueBroker(String name) {
+		this.broker = new Broker(name);
+		this.messagequeue = new HashMap<Integer, MessageQueue>();
 	}
 
 	MessageQueue accept(int port) {
+		try {
+			Channel channel = this.broker.accept(port);
+			MessageQueue mqueue = new MessageQueue(channel);
+			messagequeue.put(port, mqueue);
+			return mqueue;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
+		
 	}
 
 	MessageQueue connect(String name, int port) {
-		return null;
+		try {
+			Channel channel = this.broker.connect(name, port);
+			MessageQueue mqueue = new MessageQueue(channel);
+			messagequeue.put(port,mqueue);
+			return mqueue;
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 
 }
